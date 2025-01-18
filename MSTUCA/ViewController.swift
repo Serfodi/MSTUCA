165272c264b6eca604c5a0e26eab59d519eb37e4
Serfodi <65454204+Serfodi@users.noreply.github.com>
2025-01-18 19:50:34 +0300

    Initial Commit

---
+++ MSTUCA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSTUCA
+//
+//  Created by Сергей Насыбуллин on 18.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
